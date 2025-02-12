1f4d15236f4b0eec61f06c9dea45006c3fe62b9f
Raul Gutierrez <raul.gutialo@gmail.com>
2025-02-12 19:52:37 +0100

    Renamed default main application class name

--- src/main/java/com/rates/demo/RatesApplication.java
+++ src/main/java/com/rates/demo/RatesApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class RatesApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(RatesApplication.class, args);
 	}
 
 }
